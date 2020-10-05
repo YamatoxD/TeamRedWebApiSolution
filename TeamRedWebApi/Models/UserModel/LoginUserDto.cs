014ad5e9265584d72baad74e5a2e0d8ddcdcfe93
Victor <iuaers09809@gmail.com>
2020-10-05 12:42:45 +0200

    Account fixes

---
+++ TeamRedWebApi/Models/UserModel/LoginUserDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamRedWebApi.Models.UserModel
+{
+    public class LoginUserDto
+    {
+        [MinLength(3, ErrorMessage = "Username length must be longer then 2")]
+        [Required(ErrorMessage = "Username is required")]
+        [DataType(DataType.Text)]
+        public string UserName { get; set; }
+
+        [MinLength(3, ErrorMessage = "Password length must be longer then 2")]
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
--- TeamRedWebApi/Startup.cs
+++ TeamRedWebApi/Startup.cs
@@ -33,8 +33,8 @@ namespace TeamRedWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var key = "this is my custom Secret key for authnetication";
-
+            var key = Configuration["JWT:Secret"]; /*"this is my custom Secret key for authnetication";*/
+            
             services.AddScoped<IRealEstateRepo, RealEstateRepo>();
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
