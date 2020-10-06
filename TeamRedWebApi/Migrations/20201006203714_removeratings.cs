3fb260b429d410e149d19a7315c65a67ce303cf5
Rebin Asaad <rebinasaad8@gmail.com>
2020-10-06 22:38:32 +0200

    remove ratings

---
+++ TeamRedWebApi/Migrations/20201006203714_removeratings.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TeamRedWebApi.Migrations
+{
+    public partial class removeratings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AverageRating",
+                table: "RealEstates");
+
+            migrationBuilder.DropColumn(
+                name: "Ratings",
+                table: "RealEstates");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AverageRating",
+                table: "RealEstates",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Ratings",
+                table: "RealEstates",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
